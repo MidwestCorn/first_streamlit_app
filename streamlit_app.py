7a4b85a6901b7c07dedb74354a110b44cba85a76
MidwestCorn <121051894+MidwestCorn@users.noreply.github.com>
2022-12-20 17:32:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 #import pandas
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -26,6 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+#create the repeatable code block called a function
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -41,8 +41,10 @@ try:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)   
     
-  #except URLError as e:
-       #streamlit.error()
+ streamlit.stop() 
+    
+  except URLError as e:
+       streamlit.error()
 
 streamlit.stop()
 
