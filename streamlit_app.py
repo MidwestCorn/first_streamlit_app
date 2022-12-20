d07a1890cf020310441f02ca1c10e9890a95b738
MidwestCorn <121051894+MidwestCorn@users.noreply.github.com>
2022-12-20 17:16:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,7 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
+#New selection to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -40,8 +41,8 @@ try:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)   
     
-  except URLError as e:
-        streamlit.error()
+  #except URLError as e:
+   #     streamlit.error()
 
 streamlit.stop()
 
