6aa7dbb40d815c9bd6dc53569757c4697aeb0161
MidwestCorn <121051894+MidwestCorn@users.noreply.github.com>
2022-12-20 17:40:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,6 @@ try:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)   
     
- streamlit.stop() 
     
   except URLError as e:
        streamlit.error()
