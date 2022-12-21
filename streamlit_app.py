ff4cf31bbb2e35781360fbfd7b7c0098052a6bd7
MidwestCorn <121051894+MidwestCorn@users.noreply.github.com>
2022-12-21 16:39:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,10 @@ try:
        back_from_function = get_fruityvice_data(fruit_choice) 
        streamlit.dataframe(back_from_function)
 
-#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-#streamlit.write('The user entered ', fruit_choice)
+        streamlit.stop()
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 #import request
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
